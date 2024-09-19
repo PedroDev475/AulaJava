b246ea15e4ffc0a4debd8295df9c488071e21310
Aluno <pedrosantos471572@gmail.com>
2024-09-19 17:19:01 -0300

    exemplos aula 5

---
+++ src/Exemplos.java
@@ -0,0 +1,15 @@
+public class Exemplos {
+    public static void main(String[] args) {
+
+        float idade = 15f;
+        while (idade <= 18) {
+            System.out.println(idade);
+            idade = idade + 1;
+
+        }
+     
+
+
+
+    }
+}
